71b59f2960e245503a96edd0d758b9a3a426a28d
Arseny <62566543+BAUMANEZ@users.noreply.github.com>
2022-05-10 21:13:41 +0300

    preparations for work

---
+++ Gas Dynamics/tutorial.f95
@@ -0,0 +1,9 @@
+program tutorial
+  implicit none 
+  real, parameter :: PI = 3.1415
+  real :: rnum_1 = 0.0, rnum_2 = 0.0
+  double precision :: dbl_num = 666*1e-4
+
+  print *, dbl_num
+
+end program tutorial
