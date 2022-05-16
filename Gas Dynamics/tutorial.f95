ade0a7ef760b58c4e9c083c703026eba874d25de
Arseny <62566543+BAUMANEZ@users.noreply.github.com>
2022-05-16 16:06:23 +0300

    removed test files

--- Gas Dynamics/tutorial.f95
+++
@@ -1,9 +0,0 @@
-program tutorial
-  implicit none 
-  real, parameter :: PI = 3.1415
-  real :: rnum_1 = 0.0, rnum_2 = 0.0
-  double precision :: dbl_num = 666*1e-4
-
-  print *, dbl_num
-
-end program tutorial
