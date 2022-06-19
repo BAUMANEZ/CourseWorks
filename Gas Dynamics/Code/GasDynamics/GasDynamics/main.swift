7d7113056423a108cb1fdf057fc81018ad527e95
Arseny <62566543+BAUMANEZ@users.noreply.github.com>
2022-06-20 01:51:56 +0300

    report

--- Gas Dynamics/Code/GasDynamics/GasDynamics/main.swift
+++ Gas Dynamics/Code/GasDynamics/GasDynamics/main.swift
@@ -11,9 +11,15 @@ let c: Double = 1.0
 let h: Double = 1.0
 let profile: Advection1D.Profile = .cos
 
-let ppm = Advection1D.PPM(c: c, h: h, profile: profile)
-let ppml = Advection1D.PPML(c: c, h: h, profile: profile)
+//let ppm = Advection1D.PPM(c: c, h: h, profile: profile)
+//let ppml = Advection1D.PPML(c: c, h: h, profile: profile)
 
-//for h in [1.0, 0.5, 0.25, 0.125, 0.0625] {
-//    print(Advection1D.PPML(c: c, h: h, profile: .cos).normL2)
-//}
+print("PPM")
+for h in [1.0, 0.5, 0.25, 0.125, 0.0625] {
+    print(Advection1D.PPM(c: c, h: h, profile: profile).normL2)
+}
+print("----")
+print("PPML")
+for h in [1.0, 0.5, 0.25, 0.125, 0.0625] {
+    print(Advection1D.PPML(c: c, h: h, profile: profile).normL2)
+}
